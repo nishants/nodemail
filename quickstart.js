447091c91ab13105e76ba369a9a4dfbed4d6181f
nishant <nishant.singh87@gmail.com>
2016-02-22 18:50:04 +0530

    refactoring.

--- quickstart.js
+++ quickstart.js
@@ -11,16 +11,21 @@ var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
 
+
+var run = function(){
 // Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Gmail API.
-  authorize(JSON.parse(content), listLabels);
-});
+  fs.readFile('client_secret.json', function(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Gmail API.
+    authorize(JSON.parse(content), listLabels);
+  });
+};
+
+run();
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
